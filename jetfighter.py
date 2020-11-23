03034d862de4706d991974ed1e0b87b5e631dfc2
supahmariozuniga <74891797+supahmariozuniga@users.noreply.github.com>
2020-11-22 21:20:46 -0800

    Add files via upload

---
+++ jetfighter.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+
+FPS = pygame.time.Clock()
+FPS.tick(60)
+
+red = (25, 100, 0)
+
+display = pygame.display.set_mode((500, 500))
+display.fill(red)
+pygame.display.set_caption("hello")
+
+
+class Player1:
+    lives = 5
+
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
