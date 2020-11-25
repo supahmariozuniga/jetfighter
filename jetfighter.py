b2b4ee4e5fee55ab6d6b21d1348dfc224cd211ba
supahmariozuniga <74891797+supahmariozuniga@users.noreply.github.com>
2020-11-24 21:23:28 -0800

    Add files via upload

--- jetfighter.py
+++ jetfighter.py
@@ -2,13 +2,10 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-FPS = pygame.time.Clock()
-FPS.tick(60)
 
-red = (100, 100, 100)
-
-display = pygame.display.set_mode((500, 500))
-display.fill(red)
+height = 500
+width = 500
+display = pygame.display.set_mode((height, width))
 pygame.display.set_caption("hello")
 
 
@@ -17,28 +14,18 @@ class player(pygame.sprite.Sprite):
 
     def __init__(self, left, right, img):
         super(player, self).__init__()
-        self.image = pygame.image.load(img)
-        self.surface = pygame.Surface((26, 26))
+        self.surface = pygame.image.load(img).convert_alpha()
         self.rect = self.surface.get_rect()
         self.left = left
         self.right = right
 
-    # def update(self):
-    #     if pressed[self.left]:
-    #         self.rect.move_ip(-5, 0)
-    #     if pressed[self.right]:
-    #         self.rect.move_ip(5, 0)
-
+    def update(self):
+        if pressed[self.left]:
+            self.surface.move_ip(-5, 0)
 
 
 player1 = player(K_LEFT, K_RIGHT, "jet1.png")
 player2 = player(K_a, K_d, "jet2.png")
-
-
-
-
-
-
 
 
 open = True
@@ -53,16 +40,12 @@ while open:
 
     player.update(pressed)
 
-    display.blit(player1.surface, player1.rect)
-    display.blit(player2.surface, player2.rect)
+    display.blit(player1.surface, (width/4, height/4))
+    display.blit(player2.surface, (.75 * width, .75 * height))
 
     pygame.display.flip()
 
-
-    if pygame.key.get_pressed()[K_UP]:
-        display.fill((50, 0, 0))
+    if pressed[K_UP]:
+        display.fill((0, 5, 50))
         pygame.draw.circle(display, (0, 0, 255), (250, 250), 75)
-    # for event in pygame.event.get():
-    #     if event.type == QUIT:
-    #         pygame.quit()
-    #         sys.exit()
+        print(player2.left)
